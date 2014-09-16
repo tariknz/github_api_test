6b98a64a2d31d17119bc8c14afd09a88b284e20f
tarik <tarik.nzl@gmail.com>
2014-09-16 11:07:20 +0100

    testing github api

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,11 @@
 /* Controllers */
 
 angular.module('myApp.controllers', ['myApp.services', 'myApp.config'])
-	.controller('MainCtrl', ['$scope', 'utils', function($scope, utils) {
+	.controller('MainCtrl', ['$scope', 'utils', 'github', function($scope, utils, github) {
 
+		github.getReposForUser('test').then(function(data){
+			console.log(data);
+			$scope.test = data;
+		});
 
 	}]);
--- js/services.js
+++ js/services.js
@@ -3,4 +3,27 @@
 /* Services */
 
 angular.module('myApp.services', ['myApp.config', 'myApp.utils'])
-	.value('version', '0.1');
+	.value('version', '0.1')
+	.factory('github', ['$http', '$q', function($http, $q) {
+
+	var webServiceURL = 'https://api.github.com';
+
+	return {
+		getReposForUser: function(username) {
+			var deferred = $q.defer();
+			$http({
+				url: webServiceURL + '/repos/octokit/octokit.rb',
+				method: "GET",
+				params: { }
+			}).
+			success(function(data, status) {
+				deferred.resolve(data);
+			}).error(function(data, status){
+				handleError(status);
+				deferred.reject(data);
+			});
+
+			return deferred.promise;
+		}
+	}
+}]);
